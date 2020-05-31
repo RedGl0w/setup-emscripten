f148aa380a953d50016e9c3f07c48f339cc99134
Joachim LF <joachimlf@pm.me>
2020-05-31 19:19:10 +0200

    Fixed issue : no such file or directory, open '/home/runner/.emscripten'

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ async function run() {
     await exec.exec('emsdk', ['install', sdk])
     await exec.exec('emsdk', ['activate', sdk])
 
+
     const emConfPath = path.join(emSdk, '.emscripten')
     core.exportVariable('EM_CONFIG', emConfPath)
     const emConf = fs.readFileSync(emConfPath).toString()
-    await exec.exec('cat', [emSdk + '/.emscripten'])
+
     const emRoot = emConf.match(/EMSCRIPTEN_ROOT = '(.*)'/)[1]
     core.addPath(emRoot)
     const emNode = emConf.match(/NODE_JS = '(.*)'/)[1]
